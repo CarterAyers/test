0d405f48bffe7380834408d10c7681384acbabd9
wayers1 <wayers1@COR235-TL16>
2025-01-16 12:07:38 -0600

    Add project files.

---
+++ Project1/FileName.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "testing for github" << endl;
+	//cout << "pulling from github" << endl; // corrected cout statement
+	//cout << "Text 123" << endl;
+	//cout << "Text 123" << endl;
+}
